1a273a827b0719ac929a57bfc438864a0b1c2827
chak2156 <chak2156@gmail.com>
2017-01-01 19:09:32 +0900

    3일차

--- workspace/02_db-basic/SQL/3일차exam.sql
+++ workspace/02_db-basic/SQL/3일차exam.sql
@@ -53,12 +53,83 @@ having sum(e.salary) >= 30000
 order by sum(e.salary) desc;
 
 --7
-select last_name,salary
-from EMPLOYEES e,employees m
-outer join
 
-select e.last_name, nvl(m.last_name,'관리자 없음')
+select e.last_name,e.salary
 from employees e
-left outer join
-employees m
-on e.manager_id = m.employee_id;
+inner join EMPLOYEES m
+on e.manager_id = m.employee_id
+where e.salary>m.salary;
+
+
+
+select e.last_name,e.salary
+from EMPLOYEES e
+inner join employees m
+on e.email = m.email
+where e.salary > (select m.salary
+from employees m
+where e.manager_id = m.employee_id);
+
+--8
+select e.employee_id,e.first_name,d.department_name,m.first_name
+from employees e
+inner join departments d 
+on e.department_id = d.department_id
+inner join employees m
+on e.manager_id = m.employee_id
+inner join jobs j
+on e.job_id = j.job_id;
+
+--9
+select e.EMPLOYEE_id, e.First_name, nvl(m.First_name,'관리자 없음')
+from employees e
+left outer join employees m
+on e.manager_id =m.employee_id
+where e.First_name like '%v%';
+
+--10
+select e.EMPLOYEE_id, e.last_name, e.hire_date, m.hire_date
+from employees e
+left outer join employees m
+on e.manager_id =m.employee_id
+where e.hire_date < m.hire_date
+order by e.hire_date ;
+
+--11
+select manager_id,first_name,count(*)
+from employees
+group by manager_id
+
+select *
+from employees
+
+--22
+
+select m.employee_id,count(*)
+from employees e
+inner join EMPLOYEES m
+on e.manager_id = m.employee_id
+inner join departments d 
+on e.department_id = d.department_id
+inner join LOCATIONS l
+on d.location_id = l.location_id
+inner join countries c
+on l.country_id = c.country_id
+group by m.employee_id
+
+select *
+from employees e
+inner join EMPLOYEES m
+on e.manager_id = m.employee_id
+
+select c.country_name,count(*)
+from employees e
+inner join EMPLOYEES m
+on e.manager_id = m.employee_id
+inner join departments d 
+on e.department_id = d.department_id
+inner join LOCATIONS l
+on d.location_id = l.location_id
+inner join countries c
+on l.country_id = c.country_id
+group by c.country_name
